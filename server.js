793aaa1326b169b08186ca3f6e97dc4c7d403146
JOY <joy@mail.com>
2020-02-11 12:04:48 +0600

    Nextjs and Server Added

--- server.js
+++ server.js
@@ -14,3 +14,34 @@ const app = next({ dev });
 const handle = app.getRequestHandler();
 
 const { SHOPIFY_API_SECRET_KEY, SHOPIFY_API_KEY } = process.env;
+
+app.prepare().then(() => {
+  const server = new Koa();
+  server.use(session({ secure: true, sameSite: "none" }, server));
+  server.keys = [SHOPIFY_API_SECRET_KEY];
+
+  server.use(
+    createShopifyAuth({
+      apiKey: SHOPIFY_API_KEY,
+      secret: SHOPIFY_API_SECRET_KEY,
+      scopes: ["read_products"],
+      afterAuth(ctx) {
+        const { shop, accessToken } = ctx.session;
+
+        ctx.redirect("/");
+      }
+    })
+  );
+
+  server.use(verifyRequest());
+  server.use(async ctx => {
+    await handle(ctx.req, ctx.res);
+    ctx.respond = false;
+    ctx.res.statusCode = 200;
+    return;
+  });
+
+  server.listen(port, () => {
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+});
